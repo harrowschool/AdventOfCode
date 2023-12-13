eb06c99d61acb77acb2d5f82921cc00324d4e480
Li, Qingfeng (Andy) <119661578+Li-Qingfeng@users.noreply.github.com>
2023-12-13 02:34:52 +0000

    renamed sol

--- 2023/day9/part 2.py
+++
@@ -1,2 +0,0 @@
-from scipy.interpolate import barycentric_interpolate
-print(round(sum([barycentric_interpolate(range(len(sequence)),sequence,-1)for sequence in [[int(j)for j in i.strip().split(" ")]for i in open("9/input.txt").readlines()]])))
