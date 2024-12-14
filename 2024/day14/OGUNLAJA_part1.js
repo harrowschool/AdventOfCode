33e3aa016dac0780df8be5c3637ae8877a4e3512
Jesse Ogunlaja <130219943+JesseOgunlaja@users.noreply.github.com>
2024-12-14 18:46:58 +0000

    Day 14 Part 1 Solution

---
+++ 2024/day14/OGUNLAJA_part1.js
@@ -0,0 +1,58 @@
+import { getInput } from "./input.js";
+
+function main(inputString) {
+  const input = inputString.trim().split("\n");
+
+  const width = 100;
+  const height = 102;
+
+  const map = [];
+
+  input.forEach((robot) => {
+    const location = getLocation(robot);
+    const velocity = getVelocity(robot);
+    for (let i = 0; i < 100; i++) {
+      let newX = location[0] + velocity[0];
+      let newY = location[1] + velocity[1];
+
+      if (newX < 0) newX = width + newX + 1;
+      if (newX > width) newX = newX - width - 1;
+
+      if (newY < 0) newY = height + newY + 1;
+      if (newY > height) newY = newY - height - 1;
+
+      location[0] = newX;
+      location[1] = newY;
+    }
+    map.push(location);
+  });
+
+  let topLeftQuadrant = 0;
+  let topRightQuadrant = 0;
+  let bottomLeftQuadrant = 0;
+  let bottomRightQuadrant = 0;
+  map.forEach(([x, y]) => {
+    const widthMiddle = width / 2;
+    const heightMiddle = height / 2;
+    if (x === widthMiddle || y === heightMiddle) return;
+
+    if (x < widthMiddle && y < heightMiddle) topLeftQuadrant++;
+    if (x > widthMiddle && y < heightMiddle) topRightQuadrant++;
+    if (x < widthMiddle && y > heightMiddle) bottomLeftQuadrant++;
+    if (x > widthMiddle && y > heightMiddle) bottomRightQuadrant++;
+  });
+  console.log(
+    topLeftQuadrant *
+      topRightQuadrant *
+      bottomLeftQuadrant *
+      bottomRightQuadrant
+  );
+}
+
+function getLocation(robot) {
+  return robot.split(" ")[0].split("=")[1].split(",").map(Number);
+}
+
+function getVelocity(robot) {
+  return robot.split("=")[2].trim().split(",").map(Number);
+}
